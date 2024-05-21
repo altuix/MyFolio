38ae2c11e9a819dc158be93d8449ec471438feaa
Altuğ Gökoğlu <altuggokoglu@gmail.com>
2024-05-21 22:51:49 +0300

    tailwind custom colors and fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import logo from '../../assets/myfolio.png'
+
 
 function Home() {
   return (
-    <div className='text-3xl font-bold underline'>Home</div>
+    <div className='flex'>
+      <div>
+        <img className='' src={logo}/>
+        <h1 className='font-primary text-primary'>Frontend Developer, AI artist , and amateur Seppuku user.</h1>
+      </div>
+      <div>
+        <img src={logo}/>
+       
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'primary': '#27272a',
+        'secondary': '#65656d',
+        'tertiary-color': '#acacb4',
+        'quaternary-color': '#e4e4e7',
+        'link-color': '#14b8a6'
+      },
+      fontFamily: {
+        'primary': ['Heebo', "sans-serif"],
+        'secondary': ['Roboto', "sans-serif"],
+        // Add more custom font families as needed
+      },
+    },
   },
   plugins: [],
 }
