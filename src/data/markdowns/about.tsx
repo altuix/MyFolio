c1cf2d455569802270906c06c1d477ee5e5ceeb2
Altuğ Gökoğlu <altuggokoglu@gmail.com>
2024-06-04 19:53:52 +0300

    md file reader added, about page data edited

---
+++ src/data/markdowns/about.tsx
@@ -0,0 +1,77 @@
+let AboutMd = `
+![screenshot](src/assets/cup.jpg)
+# About Altuğ Gökoğlu
+
+Hello! I'm Altuğ Gökoğlu, a dedicated and experienced Web Developer with a rich history of over nine years in smart TV application and web development. My passion for coding and problem-solving drives me to write clean, efficient code and stay abreast of the latest technological advancements.
+
+## Professional Experience
+
+### Erstream Video Delivery Corp.
+**Frontend Team Leader**  
+*April 2021 - Present*
+
+- Spearheaded the Smart TV Development team, overseeing comprehensive project management and deployment.
+- Successfully developed and maintained over 20 Smart TV apps utilizing cutting-edge technologies such as HTML, CSS, and JavaScript.
+- Engineered a component-based framework with Vanilla JavaScript, significantly streamlining the development process for various TV brands.
+- **Key Projects:**
+  - **D-SMART GO (Smart TV App)**
+  - **EXXEN (Smart TV App)**
+  - **ROYA (Smart TV App)**
+  - **JRTV (React.js Website)**
+  - **Player Framework (In-house)**
+  - **Tv App Maker (In-house)**
+
+**Web Developer**  
+*July 2018 - April 2021*
+
+- Developed robust web and smart TV applications for a diverse clientele.
+- Leveraged technologies such as ASP.NET MVC, React.js, and jQuery to deliver high-quality solutions.
+- Created a unified structure for web and smart TV player integrations.
+
+### Deha Soft
+**Web Developer**  
+*July 2015 - December 2015*
+
+- Delivered e-commerce solutions using OpenCart for various brands.
+- Played an integral role in developing PHP-based websites and managing database-driven projects.
+
+### Dominox Studio
+**Web Developer**  
+*June 2014 - September 2014*
+
+- Executed numerous web projects using WordPress, ensuring functionality and user experience.
+
+### Marlet Advertising Studios
+**Web Developer**  
+*June 2013 - August 2013*
+
+- Developed front-end designs for web projects using HTML and CSS, focusing on aesthetics and usability.
+
+## Skills and Technologies
+
+- **Programming Languages:** JavaScript, HTML, CSS, SASS
+- **Frameworks and Libraries:** React.js, jQuery, ASP.NET MVC
+- **Development Tools:** Git, Jira, Marvelapp, Zeplin, Overflow
+- **Smart TV Development Kits:** webOS, Tizen
+- **Video Player Integration:** Various libraries and frameworks
+- **Other Skills:** Object-Oriented Programming (OOP), Unity Game Programming
+
+## Education
+
+- **Management Information Systems, Anadolu University**  
+  *2017 - 2023*
+- **Computer Programming, Istanbul University**  
+  *2014 - 2017*
+- **Web Programming, Şişli Anatolian Technical High School**  
+  *2010 - 2014*
+- **Unity Game Programming, Üçüncü Binyıl Academy**  
+  *January 2021 - June 2021*
+
+## Certifications
+
+- [Unity Game Programming, Üçüncü Binyıl Academy]
+
+`;
+
+
+export default AboutMd
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,31 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import logo from '../../assets/myfolio.png'
-import myhead from '../../assets/my_head.jpg'
+import logo from '../../assets/cup.jpg'
+
 import './style.css'
-import ProjectBox from '../../components/project-box'
-import ArticleRectangle from '../../components/article-rectangle'
-import WorkBox from '../../components/work-box'
-import TechStack from '../../components/tech-stack'
+import MarkdownPreview from '@uiw/react-markdown-preview'
+import AboutMd from '../../data/markdowns/about'
 import { deviantart, github, linkedin } from '../../assets/social'
 
 const About = () => {
   return (
     <>
-      <div id='about' className=' mb-8 flex  flex-col-reverse lg:flex-row'>
+      <div id='about' className=' mb-8 flex  flex-col-reverse '>
         <div className='flex-1 pr-5'>
-          <h1 className='mb-4 font-secondary text-6xl font-extrabold  text-primary'>
-            Frontend Developer, AI artist and amateur Seppuku user.
-          </h1>
           <p className='font-primary text-lg text-secondary'>
-            I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and
-            reliable web applications using various frameworks and technologies. I enjoy solving complex problems and
-            learning new skills. I am passionate about creating high-quality code that follows best practices and
-            industry standards. I am always looking for new challenges and opportunities to grow as a developer.
+            <MarkdownPreview
+              source={AboutMd}
+              wrapperElement={{
+                'data-color-mode': 'light'
+              }}
+              style={{ padding: 16 }}
+            />
           </p>
         </div>
-        <div className='items-center justify-center self-center p-12'>
-          <img className='w-full -rotate-3 rounded-xl transition' src={logo} />
-        </div>
+       
       </div>
       <div className='mt-5 flex items-center justify-center gap-6 opacity-70 sm:justify-start'>
         <img className='h-8 w-auto' src={github} />
